9277159ff133ce35725b1f0037e594b6733d08a0
Berlin <tommyonik@gmail.com>
2024-01-22 18:36:58 -0500

    movie poster is actually visible in search results

--- controllers/movies.js
+++ controllers/movies.js
@@ -1,13 +1,44 @@
 import { Movie } from "../models/movie.js";
 
 
-function newMovie(req,res){
-  res.render('movies/new', {
+function search(req,res){
+  res.render('movies/search', {
     title: 'Search Movie',
     movieData: null,
   })
 }
 
-export{
-  newMovie as new,
+
+
+function searchMovie(req,res){
+  console.log('🚨searchMovie Invoked');
+  console.log(req.body.movieTitle);
+  fetch(`https://api.themoviedb.org/3/search/movie?query=${req.body.movieTitle}&api_key=${process.env.TMDB_API_KEY}`)
+  .then(apiResponse => {
+    apiResponse.json()
+    .then(movieData => {
+      console.log(movieData);
+      res.render('movies/searchResults', {
+        results: movieData.results,
+        title: 'Search Results',
+      })
+    })
+  })
 }
+
+
+// async function searchMovie(req,res){
+//   const apiResponse = await fetch(`https://api.themoviedb.org/3/search/movie?query=${req.body.movieTitle}&api_key=${process.env.TMDB_API_KEY}`)
+//   const movieData = await apiResponse.json()
+//   const newMovieDoc =
+
+// }
+
+
+export{
+  search,
+  searchMovie
+}
+
+
+
--- routes/movies.js
+++ routes/movies.js
@@ -1,16 +1,13 @@
 import { Router } from 'express'
 import * as moviesCtrl from '../controllers/movies.js'
-import * as apiCtrl from '../controllers/api.js'
 
 const router = Router()
 
-// router.get('/', function (req, res) {
-//   res.render('index', { title: 'Home Page' })
-// })
 
-router.get('/new', moviesCtrl.new)
 
-router.post('/', apiCtrl.searchMovie)
+router.get('/search', moviesCtrl.search)
+
+router.post('/searchResults', moviesCtrl.searchMovie)
 
 
 
