a0627d0af52984289c99eb78cccefadea133061e
Berlin <tommyonik@gmail.com>
2024-01-23 10:38:09 -0500

    create form to display selected search result

--- controllers/movies.js
+++ controllers/movies.js
@@ -1,6 +1,12 @@
 import { Movie } from "../models/movie.js";
 
 
+function newForm(req, res){
+  console.log(req.params.apiId);
+
+}
+
+
 function search(req,res){
   res.render('movies/search', {
     title: 'Search Movie',
@@ -27,18 +33,25 @@ function searchMovie(req,res){
 }
 
 
-// async function searchMovie(req,res){
-//   const apiResponse = await fetch(`https://api.themoviedb.org/3/search/movie?query=${req.body.movieTitle}&api_key=${process.env.TMDB_API_KEY}`)
-//   const movieData = await apiResponse.json()
-//   const newMovieDoc =
 
-// }
 
 
 export{
   search,
-  searchMovie
+  searchMovie,
+  newForm
 }
 
 
 
+// now that i have the search results page:
+// i want to render a new form view     
+// then display the specific movie details from previous selection
+//    to do that I have to pass the movie Id (from the API) to run a new query
+//    new query: https://api.themoviedb.org/3/movie/{movie_id}?api_key=YOUR_API_KEY&append_to_response=credits
+
+// ---
+
+// have the results from the new query already populated 
+// have a button to add a rating/score
+// save information to database & Movie's List 
--- routes/movies.js
+++ routes/movies.js
@@ -4,10 +4,13 @@ import * as moviesCtrl from '../controllers/movies.js'
 const router = Router()
 
 
+router.get('/new/:apiId', moviesCtrl.newForm)
 
 router.get('/search', moviesCtrl.search)
 
 router.post('/searchResults', moviesCtrl.searchMovie)
+
+
 
 
 
