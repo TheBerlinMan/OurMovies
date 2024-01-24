b78f453492bd7aea39b086448abff6c36c0f39f6
Berlin <tommyonik@gmail.com>
2024-01-24 14:04:18 -0500

    adjust show view to display data from mongoDB

--- routes/movies.js
+++ routes/movies.js
@@ -6,11 +6,11 @@ const router = Router()
 
 router.get('/', moviesCtrl.index)
 
+router.get('/search', moviesCtrl.search)
+
 router.get('/:movieId', moviesCtrl.show)
 
 router.get('/new/:apiId', moviesCtrl.new)
-
-router.get('/search', moviesCtrl.search)
 
 router.post('/searchResults', moviesCtrl.searchMovie)
 
